7ac4a52389bfed68fdd93f9b36eee5d232935399
dauphineezc <124481595+dauphineezc@users.noreply.github.com>
2024-05-12 19:48:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.className = 'heart';
+    heart.style.left = Math.random() * window.innerWidth + 'px';
+    document.body.appendChild(heart);
+
+    // Remove heart after it floats away
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+// Create a new heart every 300 milliseconds
+setInterval(createHeart, 500);
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
